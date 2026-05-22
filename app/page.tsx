5677070ab13b75dee6633a9f55e5cc546fb019ba
SCNak23 <ola@sercriancaenatural.com.br>
2026-05-22 11:10:18 -0300

    Update header subtitle text

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
           <div>
             <h1 className="font-bold text-base leading-tight">Natureza Perto de Mim</h1>
             <p className="text-xs opacity-70">
-              Praças e brincadeiras com a natureza{' '}
+              áreas naturais e brincadeiras com a natureza{' '}
               <span className="opacity-50">|</span>{' '}
               criado por{' '}
               <a
